53c77362e53c2d4bc542d600f4cb1a79cb0ce868
Pedro Acevedo <thepeterace@gmail.com>
2023-01-02 12:33:50 -0400

    Add Author Routes

---
+++ routes/authors.js
@@ -0,0 +1,48 @@
+const { render } = require('ejs')
+const express = require('express')
+const author = require('../models/author')
+const router = express.Router()
+const Author = require('../models/author')
+//get all authors
+router.get('/', async (req, res) =>{
+    let searchOption = {}
+    if (req.query.name !== null && req.query.name !== ''){ 
+        // The i makes it key insensitive
+        searchOption.name = new RegExp(req.query.name, 'i')
+    }
+    try{
+        const authors = await Author.find(searchOption)
+        res.render('authors/index', { 
+            authors: authors,
+            searchOption: req.query
+        })
+    } catch{
+        res.redirect('/')
+    }
+})
+
+// new author
+router.get('/new', (req, res) =>{
+    res.render('authors/new', { author: new Author() })
+})
+
+// Create author
+router.post('/', async (req, res) =>{
+    const author = new Author({
+        name: req.body.name
+    })
+
+    try{
+        const newAuthor = await author.save()
+        // res.redirect(`/authors/${newAuthor.id}`)
+        res.redirect('/authors')
+    } catch{
+        res.render('authors/new', {
+            author: author,
+            errorMessage: 'Could not save author'
+        })
+    }
+})
+
+
+module.exports = router
--- server.js
+++ server.js
@@ -5,17 +5,21 @@ if(process.env.NODE_ENV !== 'production'){
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
-
+const bodyParser = require('body-parser')
+// Routes
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
-
+const { urlencoded } = require('body-parser')
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 // Mongo Connection
 const mongoose = require('mongoose')
+
 mongoose.set('strictQuery', false)
 mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true})
 const db = mongoose.connection
@@ -23,4 +27,5 @@ db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to mongo'))
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 app.listen(process.env.PORT || 4500, () => console.log('Server Connected'))
